4dabbc8032c7ead5189432128ae9b3ca08c0c492
Mazen Yasser <160043731+MazenYasser1@users.noreply.github.com>
2024-06-14 23:23:06 +0300

    Add files via upload

---
+++ mazen.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Mazen(object):
+    MAZEN = os.environ.get("MIZO", "")
+    
+    API_ID = os.environ.get("API_ID", "27455984")
+    
+    API_HASH = os.environ.get("API_HASH", "62d5f68ce2e9189636967120220f5755")
+
+    
